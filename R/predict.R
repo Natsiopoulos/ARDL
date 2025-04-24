a97cca6cf2ed7291952716f7185251787906c77b
Daniel Finnan <dan@custom-made.org.uk>
2025-04-24 18:17:13 +0200

    need to complete doc

--- R/predict.R
+++ R/predict.R
@@ -1,41 +1,95 @@
 #' ARDL prediction
 #'
 #' Makes predictions based on fitted ARDL models, extending the 
-#' \code{\link[stats]{predict}} generic function for the 
-#' \code{\link[ARDL-package]}. It is designed to take advantage of lags of the 
-#' dependent variable from the fitted model and new values of the independent
+#' generic function \code{\link[stats]{predict}} for use with 
+#' \code{\link{ardl}}. It is designed to take advantage of lags of the 
+#' dependent variable from the fitted model and new values of the independent 
 #' variable(s) to calculate predictions.
 #' 
-#' @param object A fitted 'ardl' model of \code{\link[base]{class}} 'ardl',
-#' created using \code{ardl}.
-#' @param newdata A time series object (e.g., "ts", "zoo" or "zooreg") or a 
-#' data frame containing the new values of \eqn{x} required to make 
-#' predictions.
-#' @param se.fit A switch indicating if standard errors are required.
+#' @param object A fitted model of \code{\link[base]{class}} \code{ardl}.
+#' @param newdata A time series object (\code{\link[stats]{ts}},
+#' \code{\link[zoo]{zoo}} or \code{\link[zoo]{zooreg}}) or a 
+#' \code{\link[base]{data.frame}} containing the new values of \eqn{x} required
+#' to make predictions.
 #' 
-#' @return \code{predict} returns predictions of \eqn{y}.
+#' @details
+#' \code{predict.ardl} works recursively to calculate predictions based on \eqn{y_{t-p}} values
+#' contained within a fitted model, and new values of \eqn{x} supplied through the
+#' \code{newdata} argument. It supports the 5 different deterministic cases,
+#' involving different restrictions on the intercept and/or trend
+#' (see \code{\link{recm}}), as well as fixed regressors (e.g. dummies). It is 
+#' important to note that for a given prediction \eqn{\hat{y_t}}, the 
+#' independent variables provided through \code{newdata}, are expected to follow 
+#' consecutively, i.e. \eqn{x_t}, \eqn{x_{t+1}}, \eqn{x_{t+2}}, with no gaps.
+#' If a time series object is passed to the function, and the underlying 
+#' \code{ardl} model contains a trend (Case 4 & 5 under PSS), the timestamps 
+#' defined in \code{newdata} will be ignored. The trend will be constructed from
+#' the existing \code{ardl} model.
 #' 
-#' @section Calculation:
-#' Details of the calculation, Kleanthis' matrices detail
-#'
+#' @section Calculation
+#'  
+#'  
+#' @return \code{predict.ardl} returns values for \eqn{\hat{y_t}} equal in length
+#' to the number of rows in \code{newdata}, as a \code{\link[base]{vector}}.
+#' 
 #' @seealso \code{\link{ardl}}
+#' @author Kleanthis Natsiopoulos, \email{klnatsio@@gmail.com}
+#' @author Daniel Finnan, \email{dan@@custom-made.org.uk}
 #' @keywords models ts
 #' @examples
-#' ## Prediction example -----
+#' data(denmark)
+#' ## Estimate an ARDL(2,1,2,1,1) model, using a subset of the denmark data
+#' ardl_21211 <- ardl(LRM ~ LRY + LPY + IBO + IDE, data = denmark,
+#'                   order = c(2,1,2,1,1),
+#'                   start = "1974 Q1", end = "1986 Q2")
+#' ## Make predictions based on the remainder of the denmark data not used in 
+#' ## training the model
+#' predict_data <- window(denmark, start = "1986 Q3", end = "1987 Q3")
+#' ## Drop the dependent variable, since we're predicting it
+#' predict_data <- predict_data[, c("LRY", "LPY", "IBO", "IDE")]
+#' predictions <- predict.ardl(ardl_21211, predict_data)
 #'
-#' # Indirectly from an ARDL
-#' ardl_3132 <- ardl(LRM ~ LRY + IBO + IDE, data = denmark, order = c(3,1,3,2))
-#' recm_3132 <- recm(ardl_3132, case = 2)
+#' ## Estimate a ARDL(4,4,4,4) model with a linear trend and dummies
+#' ## Create dummies
+#' d_74Q1_75Q3_ <- ifelse(time(denmark) >= "1974 Q1" & time(denmark) <= "1975 Q3", 1, 0)
+#' # Add them to the data
+#' denmark <- cbind(denmark, d_74Q1_75Q3)
+#' ## Estimate the model
+#' ardl_4444 <- ardl(LRM ~ LRY + LPY + IBO + trend(LRM) | d_74Q1_75Q3,
+#'                  data = denmark, 
+#'                  order = c(4,4,4,4),
+#'                  start = "1974 Q1", end = "1986 Q2")
+#' ## Take the remaining data not used in training the model
+#' predict_data <- window(denmark, start = "1986 Q3", end = "1987 Q3")
+#' ## Drop the dependent variable
+#' predict_data <- predict_data[, c("LRY", "LPY", "IBO", "d_74Q1_75Q3")]
+#' ## Compute the predictions
+#' predictions <- predict.ardl(ardl_4444, predict_data)
+#'
 #' @importFrom stats predict  
 #' @method predict ardl
 #' @export
-predict.ardl <- function(object, newdata, se.fit = FALSE, ...) {
+predict.ardl <- function(object, newdata, ...) {
+    if (missing(newdata)) {
+        stop("Need to provide some 'newdata' to predict.", call. = FALSE)
+    }
+    if (!identical(class(object), c("dynlm","lm", "ardl"))) {
+        stop("'object' needs to be a fitted model using 'ardl' method.", call. = FALSE)
+    }
+    if (any(is.na(newdata))) {
+        stop("'newdata' contains some NA values.", call. = FALSE)
+    }
+    # Check all necessary columns are in new data
+    orig_cols <- c(object$parsed_formula$x_part$var, object$parsed_formula$fixed_part$var)
+    newdata_cols <- colnames(newdata)
+    if (identical(orig_cols, newdata_cols) == FALSE) {
+        stop("'newdata' columns don't match original data.", call. = FALSE)
+    }
     # Handling simple data.frames
     if (!any(c("ts", "zoo", "zooreg") %in% class(newdata))) {
         newdata <- stats::ts(newdata, start = 1, end = nrow(newdata), 
                              frequency = 1)
     }
-    # Add check for empty cells in newdata
     kx <- object$parsed_formula$kx
     kw <- object$parsed_formula$kw
     kfixed <- object$parsed_formula$kfixed
@@ -50,7 +104,7 @@ predict.ardl <- function(object, newdata, se.fit = FALSE, ...) {
                                     w_part,
                                     object$parsed_formula$x_part$var,
                                     object$parsed_formula$fixed_part$var)]
-    model_matrix <- t(matrix(unlist(model_matrix), nrow = ncol(model_matrix), 
+    model_matrix <- t(matrix(unlist(model_matrix), nrow = ncol(model_matrix),
                              byrow = TRUE, dimnames = dimnames(t(model_matrix))))
     n_ahead <- nrow(newdata)
     if (kw == 2) {
@@ -98,5 +152,3 @@ predict.ardl <- function(object, newdata, se.fit = FALSE, ...) {
     # Return as a named num like predict.lm
     return(setNames(predictor, seq(1, n_ahead)))
 }
-# 
-# predict.ardl(object, newdata)
--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,4 +1,4 @@
-test_that("verify prediction results all cases - df", {
+test_that("verify prediction results all cases", {
     newdata <- data.frame("Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
                           "D7475"= c(0,1,0), "D7579"=c(1,0,0))
     # Case 1: No intercept and no trend
@@ -7,120 +7,88 @@ test_that("verify prediction results all cases - df", {
     case_ii_iii <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
     # Case 4 & 5: Unrestricted intercept and restricted trend / Unrestricted intercept and unrestricted trend
     case_iv_v <- ardl(w ~ Prod + UR + Wedge + Union + trend(w) | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
+    # Alternative trend methods
+    case_iv_v_time <- ardl(w ~ Prod + UR + Wedge + Union + time(w) | D7475 + D7579, order =c(3,0,1,4,3), data = ts(PSS2001))
+    case_iv_v_trend_scale <- ardl(w ~ Prod + UR + Wedge + Union + trend(w, scale = FALSE) | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
     result_case_i <- predict.ardl(case_i, newdata)
     result_case_ii_iii <- predict.ardl(case_ii_iii, newdata)
     result_case_iv_v <- predict.ardl(case_iv_v, newdata)
+    result_case_iv_v_time <- predict.ardl(case_iv_v_time, newdata)
+    result_case_iv_v_trend_scale <- predict.ardl(case_iv_v_trend_scale, newdata)
     # These are externally calculated
-    case_i_y1 <- -13.1721023693382
-    case_i_y2 <- -40.3134240474061
-    case_i_y3 <- -57.8052029786566
-    case_ii_iii_y1 <- -7.40819925882942
-    case_ii_iii_y2 <- -24.8395014334613
-    case_ii_iii_y3 <- -29.1887358923117
-    case_iv_v_y1 <- -6.61455842285156
-    case_iv_v_y2 <- -23.5381169607463
-    case_iv_v_y3 <- -27.919945003499
+    case_i <- setNames(c(-13.1721023693382, -40.3134240474061, -57.8052029786566), seq(1, 3))
+    case_ii_iii <- setNames(c(-7.40819925882942, -24.8395014334613, -29.1887358923117), seq(1, 3))
+    case_iv_v <- setNames(c(-6.61455842285156, -23.5381169607463, -27.919945003499), seq(1, 3))
     # Note expect equal ignores small numeric differences
-    expect_equal(c(case_i_y1, case_i_y2, case_i_y3), result_case_i)
-    expect_equal(c(case_ii_iii_y1, case_ii_iii_y2, case_ii_iii_y3), result_case_ii_iii)
-    expect_equal(c(case_iv_v_y1, case_iv_v_y2, case_iv_v_y3), result_case_iv_v)
+    expect_equal(case_i, result_case_i, tolerance=1e-3)
+    expect_equal(case_ii_iii, result_case_ii_iii, tolerance=1e-3)
+    expect_equal(case_iv_v, result_case_iv_v, tolerance=1e-3)
+    expect_equal(case_iv_v, result_case_iv_v_time, tolerance=1e-3)
+    expect_equal(case_iv_v, result_case_iv_v_trend_scale, tolerance=1e-3)
 })
 
-test_that("verify prediction results all cases - kn", {
+test_that("verify equivalence of different newdata types", {
+    model <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
+    # dataframe
+    newdata_df <- data.frame(
+        "Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
+        "D7475"= c(0,1,0), "D7579"=c(1,0,0))
+    # ts object, no timestamp
+    newdata_ts <- ts(newdata_df)
+    result_df <- predict.ardl(model, newdata_df)
+    result_ts <- predict.ardl(model, newdata_ts)
+    expect_equal(result_ts, result_df)
+    # ts object with timestamp
+    newdata_ts_timestamp <- ts(newdata_df, start = c(1998, 1))
+    result_ts_timestamp <- predict.ardl(model, newdata_ts_timestamp)
+    expect_equal(result_ts_timestamp, result_df)
+    # zoo object
+    newdata_zoo <- zoo(newdata_df)
+    result_zoo <- predict.ardl(model, newdata_zoo)
+    expect_equal(result_zoo, result_df)
+    # zooreg object
+    newdata_zooreg <- zooreg(newdata_df, start = c(1998, 1), frequency = 4)
+    result_zooreg <- predict.ardl(model, newdata_zooreg)
+    expect_equal(result_zooreg, result_df)
+})
+
+test_that("check integrity of newdata", {
+    # NA values
+    newdata <- data.frame("Prod" = c(NA,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
+                          "D7475"= c(0,1,0), "D7579"=c(1,0,0))
+    ardl_model <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
+    expect_error(predict.ardl(ardl_model, newdata))
+    # Empty newdata
+    expect_error(predict.ardl(ardl_model))
+    # Not ARDL model
     newdata <- data.frame("Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
                           "D7475"= c(0,1,0), "D7579"=c(1,0,0))
-    # Case 1: No intercept and no trend
-    case_i <- ardl(w ~ Prod + UR + Wedge + Union - 1 | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
-    case_i_y1 <- t(matrix(case_i$coefficients)) %*% matrix(c(3.541190, 3.528882, 3.525881,
-                                                1,
-                                                4, 1.898443,
-                                                7, -0.3640409, -0.3627026, -0.3582229, -0.3523980,
-                                                11, -0.6529655, -0.6529655, -0.6529655,
-                                                0, 1))
-    #T+1
-    #-13.1721
-    case_i_y2 <- t(matrix(case_i$coefficients)) %*% matrix(c(-13.1721, 3.541190, 3.528882,
-                                                2,
-                                                5, 4,
-                                                8, 7, -0.3640409, -0.3627026, -0.3582229,
-                                                22, 11, -0.6529655, -0.6529655,
-                                                1, 0))
-    #T+2
-    #-40.31342
-    case_i_y3 <- t(matrix(case_i$coefficients)) %*% matrix(c(-40.31342, -13.1721, 3.541190,
-                                                3,
-                                                6, 5,
-                                                9, 8, 7, -0.3640409, -0.3627026,
-                                                33, 22, 11, -0.6529655,
-                                                0, 0))
-    # Case 2 & 3: Restricted intercept and no trend / Unrestricted intercept and no trend
-    case_ii_iii <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
-    case_ii_iii_y1 <- t(matrix(case_ii_iii$coefficients)) %*% matrix(c(1,
-                                                3.541190, 3.528882, 3.525881,
-                                                1,
-                                                4, 1.898443,
-                                                7, -0.3640409, -0.3627026, -0.3582229, -0.3523980,
-                                                11, -0.6529655, -0.6529655, -0.6529655,
-                                                0, 1))
-    #T+1
-    #-7.408199
-    case_ii_iii_y2 <- t(matrix(case_ii_iii$coefficients)) %*% matrix(c(1,
-                                                -7.408199, 3.541190, 3.528882,
-                                                2,
-                                                5, 4,
-                                                8, 7, -0.3640409, -0.3627026, -0.3582229,
-                                                22, 11, -0.6529655, -0.6529655,
-                                                1, 0))
-    #T+2
-    #-24.8395
-    case_ii_iii_y3 <- t(matrix(case_ii_iii$coefficients)) %*% matrix(c(1,
-                                                -24.8395, -7.408199, 3.541190,
-                                                3,
-                                                6, 5,
-                                                9, 8, 7, -0.3640409, -0.3627026,
-                                                33, 22, 11, -0.6529655,
-                                                0, 0))
-    #T+3
-    #-29.18874
-    
-    # Case 4 & 5: Unrestricted intercept and restricted trend / Unrestricted intercept and unrestricted trend
-    case_iv_v <- ardl(w ~ Prod + UR + Wedge + Union + trend(w) | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
-    
-    case_iv_v_y1 <- t(matrix(case_iv_v$coefficients)) %*% matrix(c(1, 28.25,
-                                                3.541190, 3.528882, 3.525881,
-                                                1,
-                                                4, 1.898443,
-                                                7, -0.3640409, -0.3627026, -0.3582229, -0.3523980,
-                                                11, -0.6529655, -0.6529655, -0.6529655,
-                                                0, 1))
-    #T+1
-    #-6.616179
-    case_iv_v_y2 <- t(matrix(case_iv_v$coefficients)) %*% matrix(c(1, 28.50,
-                                                -6.616179, 3.541190, 3.528882,
-                                                2,
-                                                5, 4,
-                                                8, 7, -0.3640409, -0.3627026, -0.3582229,
-                                                22, 11, -0.6529655, -0.6529655,
-                                                1, 0))
-    #T+2
-    #-23.54048
-    case_iv_v_y3 <- t(matrix(case_iv_v$coefficients)) %*% matrix(c(1, 28.75,
-                                                -23.54048, -6.616179, 3.541190,
-                                                3,
-                                                6, 5,
-                                                9, 8, 7, -0.3640409, -0.3627026,
-                                                33, 22, 11, -0.6529655,
-                                                0, 0))
-    #T+3
-    #-27.92307
-    #####
-    result_case_i <- predict.ardl(case_i, newdata)
-    result_case_ii_iii <- predict.ardl(case_ii_iii, newdata)
-    result_case_iv_v <- predict.ardl(case_iv_v, newdata)
-    expect_equal(c(case_i_y1, case_i_y2, case_i_y3), result_case_i)
-    expect_equal(c(case_ii_iii_y1, case_ii_iii_y2, case_ii_iii_y3), result_case_ii_iii)
-    expect_equal(c(case_iv_v_y1, case_iv_v_y2, case_iv_v_y3), result_case_iv_v)
+    uecm_model <- uecm(ardl_model)
+    expect_error(predict.ardl(uecm_model, newdata))
+    # Missing cols
+    newdata <- data.frame("UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
+                          "D7475"= c(0,1,0), "D7579"=c(1,0,0))
+    expect_error(predict.ardl(ardl_model, newdata))
+    # Give newdata in another order
+    newdata <- data.frame("UR"= c(4,5,6), "Prod" = c(1,2,3), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
+                          "D7475"= c(0,1,0), "D7579"=c(1,0,0))
+    expect_error(predict.ardl(ardl_model, newdata))
+    # Give newdata with non-consecutive times
+    newdata <- data.frame("Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
+                          "D7475"= c(0,1,0), "D7579"=c(1,0,0))
+    newdata_ts <- ts(newdata, start = c(2000, 1))
+    predict.ardl(ardl_model, newdata_ts)
+    # This should ignore different timestamp and be equivalent
+    expect_equal(predict.ardl(ardl_model, newdata_ts),
+                 setNames(c(-7.40819925882942, -24.8395014334613, -29.1887358923117), seq(1, 3)))
 })
+
+# Questions
+# 
+# Note: we could just convert uecm/recm to ardl, but I wonder if this confuses things, they're not making predictions based on ardl, although we could state this in help
+# 
+# I put my name in the author, not in it for the glory! 
+
 
 
 
