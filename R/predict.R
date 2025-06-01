e62a2b98878f04f974a5aa547c2a7e0b4e778b8c
Daniel Finnan <dan@custom-made.org.uk>
2025-06-01 13:53:58 +0200

    finished new work on predict, adding checks for ts objects, more tests, following code review

--- R/predict.R
+++ R/predict.R
@@ -1,38 +1,37 @@
-#' ARDL prediction
+#' Predict method for ARDL model fits
 #'
 #' Makes predictions based on fitted ARDL models, extending the 
 #' generic function \code{\link[stats]{predict}} for use with 
-#' \code{\link{ardl}}. It is designed to take advantage of lags of the 
-#' dependent variable from the fitted model and new values of the independent 
-#' variable(s) to calculate predictions.
+#' \code{\link{ardl}}. It is designed to take advantage of lags of the dependent
+#' variable (AR components) from the fitted model and new values of the 
+#' independent variable(s) to calculate predictions.
 #' 
-#' @param object A fitted model of \code{\link[base]{class}} \code{ardl}.
+#' @param object A fitted model of \code{\link[base]{class}} \code{\link{ardl}}.
 #' @param newdata A time series object (\code{\link[stats]{ts}},
-#' \code{\link[zoo]{zoo}} or \code{\link[zoo]{zooreg}}) or a 
-#' \code{\link[base]{data.frame}} containing the new values of \eqn{x} required
-#' to make predictions.
+#' \code{\link[zoo]{zoo}}, \code{\link[zoo]{zooreg}}) or a 
+#' \code{\link[base]{data.frame}} containing the new values of the independent 
+#' variables \eqn{x} required to make predictions.
+#' @param ... further arguments passed to or from other methods.
 #' 
 #' @details
-#' \code{predict.ardl} works recursively to calculate predictions based on \eqn{y_{t-p}} values
-#' contained within a fitted model, and new values of \eqn{x_{t+1}} supplied through the
-#' \code{newdata} argument. It supports the 5 different deterministic cases,
-#' involving different restrictions on the intercept and/or trend
-#' (see \code{\link{recm}}), as well as fixed regressors (e.g. dummies). It is 
-#' important to note that for a given prediction \eqn{\hat{y}_{t+1}}, the 
-#' independent variables provided through \code{newdata}, are expected to follow 
-#' consecutively from the last observation \eqn{x_{t}} used to create the model,
-#' i.e. \eqn{x_{t+1}}, \eqn{x_{t+2}}, \eqn{x_{t+3}}, with no gaps.
-#' If a time series object is passed to the function, and the underlying 
-#' \code{ardl} model contains a trend (Case 4 & 5 under PSS), the timestamps 
-#' defined in \code{newdata} will be ignored, instead constructed from
-#' the existing \code{ardl} model.
+#' \code{predict.ardl} works recursively to calculate predictions based on
+#' \eqn{y_{t-1}, ..., y_{t-p}} values contained within a fitted model, and new 
+#' values of \eqn{x_{T+1}, ..., x_{T+h}}, where \eqn{T} denotes the last observation 
+#' of \eqn{t} from the original data and \eqn{h} is the number of new 
+#' observations that will be used to calculate predictions. These are supplied 
+#' through the \code{newdata} argument. It is important to note that for a given 
+#' prediction \eqn{\hat{y}_{T+1}}, the independent variables provided through 
+#' \code{newdata}, are expected to follow consecutively from the last observation
+#' \eqn{x_{T}} used to create the model, i.e. \eqn{x_{T+1}, ..., x_{T+h}},
+#' with no gaps. If a time series object is passed to the function, the frequency
+#' and whether the observations follow consecutively will be checked. 
 #' 
 #' @section Calculation example:
 #' 
 #' We want to predict \eqn{\hat{y}_{T+1}} given an \eqn{ARDL(2,2)} model:
 #' 
 #' \deqn{
-#'      \hat{\boldsymbol{y}}_{T+1} = Z \boldsymbol{\beta}
+#'      \hat{y}_{T+1} = Z \boldsymbol{\beta}
 #' }
 #' where \eqn{Z} is our design matrix and \eqn{\beta} is a vector of our model 
 #' coefficients. We need 2 lags for our dependent and independent variables:
@@ -46,7 +45,7 @@
 #'         & \mathbf{c} & \mathbf{y_{t-1}} & \mathbf{y_{t-2}} & \mathbf{x_t} &
 #'         \mathbf{x_{t-1}} & \mathbf{x_{t-2}} \\
 #'         \hline
-#'         T\!+\!1 & 1 & y_{t} & y_{t-1} & x_{t+1} & x_{t} & x_{t-1}
+#'         T\!+\!1 & 1 & y_{T} & y_{T-1} & x_{T+1} & x_{T} & x_{T-1}
 #'         \end{array}
 #'         \right]
 #' }
@@ -58,16 +57,16 @@
 #'     \begin{array}{c|cccccc}
 #'     & \mathbf{c} & \mathbf{y_{t-1}} & \mathbf{y_{t-2}} & \mathbf{x_t} & \mathbf{x_{t-1}} & \mathbf{x_{t-2}} \\
 #'     \hline
-#'     T\!+\!2 & 1 & y_{t+1} & y_{t} & x_{t+2} & x_{t+1} & x_{t}
+#'     T\!+\!2 & 1 & y_{T+1} & y_{T} & x_{T+2} & x_{T+1} & x_{T}
 #'     \end{array}
 #'     \right]
 #' }
 #' 
-#' Iteration continues until we've calculated the necessary values of
-#' \eqn{\hat{y}_{T+n}} for \code{newdata}.
+#' Iteration continues until we've calculated all the necessary values of
+#' \eqn{\hat{y}_{T+h}} for \code{newdata}.
 #'  
-#' @return \code{predict.ardl} returns values for \eqn{\hat{y}_{t+n}} equal in 
-#' length to the number of rows in \code{newdata}, 
+#' @return \code{predict.ardl} returns values for \eqn{\hat{y}_{T+1}, ..., 
+#' \hat{y}_{T+h}} equal in length to the number of rows in \code{newdata}, 
 #' as a \code{\link[base]{vector}}.
 #' 
 #' @seealso \code{\link{ardl}}
@@ -78,44 +77,59 @@
 #' data(denmark)
 #' ## Estimate an ARDL(2,1,2,1,1) model, using a subset of the denmark data
 #' ardl_21211 <- ardl(LRM ~ LRY + LPY + IBO + IDE, data = denmark,
-#'                   order = c(2,1,2,1,1),
-#'                   start = "1974 Q1", end = "1986 Q2")
+#'                    order = c(2,1,2,1,1),
+#'                    start = "1974 Q1", end = "1986 Q2")
 #' ## Make predictions based on the remainder of the denmark data not used in 
 #' ## training the model
 #' predict_data <- window(denmark, start = "1986 Q3", end = "1987 Q3")
 #' ## Drop the dependent variable, since we're predicting it
 #' predict_data <- predict_data[, c("LRY", "LPY", "IBO", "IDE")]
 #' predictions <- predict.ardl(ardl_21211, predict_data)
-#'
+#' 
 #' ## Estimate a ARDL(4,4,4,4) model with a linear trend and dummies
 #' ## Create dummies
 #' d_74Q1_75Q3 <- ifelse(time(denmark) >= "1974 Q1" & time(denmark) <= "1975 Q3", 1, 0)
 #' # Add them to the data
-#' denmark <- cbind(denmark, d_74Q1_75Q3)
+#' denmark_dums <- cbind(denmark, d_74Q1_75Q3)
 #' ## Estimate the model
 #' ardl_4444 <- ardl(LRM ~ LRY + LPY + IBO + trend(LRM) | d_74Q1_75Q3,
-#'                  data = denmark, 
-#'                  order = c(4,4,4,4),
-#'                  start = "1974 Q1", end = "1986 Q2")
+#'                   data = denmark_dums, 
+#'                   order = c(4,4,4,4),
+#'                   start = "1974 Q1", end = "1986 Q2")
 #' ## Take the remaining data not used in training the model
-#' predict_data <- window(denmark, start = "1986 Q3", end = "1987 Q3")
+#' predict_data <- window(denmark_dums, start = "1986 Q3", end = "1987 Q3")
 #' ## Drop the dependent variable
 #' predict_data <- predict_data[, c("LRY", "LPY", "IBO", "d_74Q1_75Q3")]
 #' ## Compute the predictions
 #' predictions <- predict.ardl(ardl_4444, predict_data)
 #'
-#' @importFrom stats predict  
 #' @method predict ardl
 #' @export
 predict.ardl <- function(object, newdata, ...) {
+    newdata_class <- class(newdata)
     if (missing(newdata)) {
         stop("Need to provide some 'newdata' to predict.", call. = FALSE)
+    }
+    if (!any(c("ts", "zoo", "zooreg", "data.frame") %in% newdata_class)) {
+        stop("'newdata' must be a 'ts', 'zoo', 'zooreg' or 'data.frame' object.", call. = FALSE)
     }
     if (!identical(class(object), c("dynlm","lm", "ardl"))) {
         stop("'object' needs to be a fitted model using 'ardl' method.", call. = FALSE)
     }
     if (any(is.na(newdata))) {
         stop("'newdata' contains some NA values.", call. = FALSE)
+    }
+    if ((any(c("ts", "zoo", "zooreg") %in% newdata_class) && (frequency(object$data) != frequency(newdata)))) {
+        stop("'newdata' needs to have the same frequency as the original data.", call. = FALSE)
+    }
+    # Consecutive series zoo/zooreg
+    # Note: we need to subset data in ardl model, since it stores complete dataset, despite start/end parameter
+    if (any(c("zoo", "zooreg") %in% newdata_class) && !((end(object$data[index(object),]) + deltat(object$data[index(object),])) == start(newdata))) {
+        stop("Observations in 'newdata' need to follow original data consecutively.", call. = FALSE)
+    }
+    # Consecutive series ts. Note: ts can't use delta date function, for simplisicity we coerce to zoo
+    if ( is.ts(newdata) && !((end(as.zoo(object$data[index(object),])) + deltat(as.zoo(object$data[index(object),]))) == start(as.zoo(newdata))) ) {
+        stop("Observations in 'newdata' need to follow original data consecutively.", call. = FALSE)
     }
     # Check all necessary columns are in new data
     orig_cols <- c(object$parsed_formula$x_part$var, object$parsed_formula$fixed_part$var)
@@ -124,7 +138,7 @@ predict.ardl <- function(object, newdata, ...) {
         stop("'newdata' columns don't match original data.", call. = FALSE)
     }
     # Handling simple data.frames
-    if (!any(c("ts", "zoo", "zooreg") %in% class(newdata))) {
+    if (!any(c("ts", "zoo", "zooreg") %in% newdata_class)) {
         newdata <- stats::ts(newdata, start = 1, end = nrow(newdata), 
                              frequency = 1)
     }
@@ -187,6 +201,11 @@ predict.ardl <- function(object, newdata, ...) {
         predictor <- c(predictor, t(coeffs) %*% matrix(design_matrix))
         model_matrix_y <- c(model_matrix_y, predictor[length(predictor)])
     }
-    # Return as a named num like predict.lm
-    return(setNames(predictor, seq(1, n_ahead)))
+    # Return data.frame if newdata isn't a time series object, otherwise return
+    # time series object with timestamps
+    if (!any(c("ts", "zoo", "zooreg") %in% newdata_class)) {
+        return(data.frame(predictions = predictor))
+    } else {
+        return(cbind(newdata, predictor) %>% .[,dim(.)[2]])
+    }
 }
--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,4 +1,5 @@
 test_that("verify prediction results all cases", {
+    # Using data.frame as the test case, time series objects tested separately
     newdata <- data.frame("Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
                           "D7475"= c(0,1,0), "D7579"=c(1,0,0))
     # Case 1: No intercept and no trend
@@ -16,15 +17,24 @@ test_that("verify prediction results all cases", {
     result_case_iv_v_time <- predict.ardl(case_iv_v_time, newdata)
     result_case_iv_v_trend_scale <- predict.ardl(case_iv_v_trend_scale, newdata)
     # These are externally calculated
-    case_i <- setNames(c(-13.1721023693382, -40.3134240474061, -57.8052029786566), seq(1, 3))
-    case_ii_iii <- setNames(c(-7.40819925882942, -24.8395014334613, -29.1887358923117), seq(1, 3))
-    case_iv_v <- setNames(c(-6.61455842285156, -23.5381169607463, -27.919945003499), seq(1, 3))
-    # Note expect equal ignores small numeric differences
+    case_i <- data.frame(predictions = c(-13.1721023693382, -40.3134240474061, -57.8052029786566))
+    case_ii_iii <- data.frame(predictions = c(-7.40819925882942, -24.8395014334613, -29.1887358923117))
+    case_iv_v <- data.frame(predictions = c(-6.61455842285156, -23.5381169607463, -27.919945003499))
+    # Note: Ignores small numeric differences
     expect_equal(case_i, result_case_i, tolerance=1e-3)
     expect_equal(case_ii_iii, result_case_ii_iii, tolerance=1e-3)
     expect_equal(case_iv_v, result_case_iv_v, tolerance=1e-3)
     expect_equal(case_iv_v, result_case_iv_v_time, tolerance=1e-3)
     expect_equal(case_iv_v, result_case_iv_v_trend_scale, tolerance=1e-3)
+})
+
+test_that("unsupported newdata object", {
+    newdata <- matrix(c(1,2,3,  4,5,6,  7,8,9,  11,22,33,  0,1,0,  1,0,0),
+                      nrow = 3,
+                      ncol = 6
+                      )
+    case_i <- ardl(w ~ Prod + UR + Wedge + Union - 1 | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001)
+    expect_error(predict.ardl(case_i, newdata))
 })
 
 test_that("verify equivalence of different newdata types", {
@@ -34,26 +44,23 @@ test_that("verify equivalence of different newdata types", {
     newdata_df <- data.frame(
         "Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
         "D7475"= c(0,1,0), "D7579"=c(1,0,0))
-    # ts object, no timestamp
-    newdata_ts <- ts(newdata_df)
-    result_df <- predict.ardl(model, newdata_df)
-    result_ts <- predict.ardl(model, newdata_ts)
-    expect_equal(result_ts, result_df)
-    # ts object with timestamp
-    newdata_ts_timestamp <- ts(newdata_df, start = c(1998, 1))
+    # setting frequency and start timestamp as expected for time series objects
+    # ts object
+    newdata_ts_timestamp <- ts(newdata_df, start = c(1998, 1), frequency = 4)
     result_ts_timestamp <- predict.ardl(model, newdata_ts_timestamp)
-    expect_equal(result_ts_timestamp, result_df)
+    expect_equal(c(result_df[,"predictions"]), c(unname(result_ts_timestamp)))
     # zoo object
-    newdata_zoo <- zoo(newdata_df)
-    result_zoo <- predict.ardl(model, newdata_zoo)
-    expect_equal(result_zoo, result_df)
+    dates <- as.yearqtr(c("1998 1", "1998 2", "1998 3"), format = "%Y %q")
+    newdata_zoo_timestamp <- zoo(newdata_df, dates)
+    result_zoo_timestamp <- predict.ardl(model, newdata_zoo_timestamp)
+    expect_equal(c(result_df[,"predictions"]), c(coredata(result_zoo_timestamp)))
     # zooreg object
-    newdata_zooreg <- zooreg(newdata_df, start = c(1998, 1), frequency = 4)
-    result_zooreg <- predict.ardl(model, newdata_zooreg)
-    expect_equal(result_zooreg, result_df)
+    newdata_zooreg_timestamp <- zooreg(newdata_df, start = c(1998, 1), frequency = 4)
+    result_zooreg_timestamp <- predict.ardl(model, newdata_zooreg_timestamp)
+    expect_equal(c(result_df[,"predictions"]), c(coredata(result_zooreg_timestamp)))
 })
 
-test_that("check integrity of newdata", {
+test_that("check integrity of newdata and ardl object", {
     # NA values
     newdata <- data.frame("Prod" = c(NA,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
                           "D7475"= c(0,1,0), "D7579"=c(1,0,0))
@@ -74,18 +81,41 @@ test_that("check integrity of newdata", {
     newdata <- data.frame("UR"= c(4,5,6), "Prod" = c(1,2,3), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
                           "D7475"= c(0,1,0), "D7579"=c(1,0,0))
     expect_error(predict.ardl(ardl_model, newdata))
-    # Give newdata with non-consecutive times
-    newdata <- data.frame("Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
-                          "D7475"= c(0,1,0), "D7579"=c(1,0,0))
-    newdata_ts <- ts(newdata, start = c(2000, 1))
-    predict.ardl(ardl_model, newdata_ts)
-    # This should ignore different timestamp and be equivalent
-    expect_equal(predict.ardl(ardl_model, newdata_ts),
-                 setNames(c(-7.40819925882942, -24.8395014334613, -29.1887358923117), seq(1, 3)))
 })
 
-
-
-
-
-
+test_that("check time series objects", {
+    PSS2001_ts <- ts(PSS2001, start = c(1970, 1), frequency = 4)
+    PSS2001_zoo <- as.zoo(window(PSS2001, start = as.yearqtr("1970 1", format = "%Y %q"), end = as.yearqtr("1997 4", format = "%Y %q")))
+    PSS2001_zooreg <- zooreg(PSS2001, start = c(1970, 1), frequency = 4)
+    
+    ardl_ts <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001_ts)
+    ardl_zoo <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001_zoo)
+    ardl_zooreg <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001_zooreg)
+    newdata <- data.frame("Prod" = c(1,2,3), "UR"= c(4,5,6), "Wedge"=c(7,8,9), "Union" = c(11,22,33),
+                          "D7475"= c(0,1,0), "D7579"=c(1,0,0))
+    # Check frequency is the same
+    # PSS2001 has frequency = 4
+    newdata_ts <- ts(newdata, start = c(1998, 1), frequency = 3)
+    expect_error(predict.ardl(ardl_ts, newdata_ts))
+    newdata_zoo <- zoo(newdata, frequency = 1)
+    expect_error(predict.ardl(ardl_zoo, newdata_zoo))
+    newdata_zooreg <- zooreg(newdata, start = c(1998, 1), frequency = 5)
+    expect_error(predict.ardl(ardl_zooreg, newdata_zooreg))
+    # Check time series are consecutive
+    newdata_ts <- ts(newdata, start = c(1997, 3), frequency = 4)
+    expect_error(predict.ardl(ardl_ts, newdata_ts))
+    dates <- as.yearqtr(c("1998 2", "1998 3", "1998 4"), format = "%Y %q")
+    newdata_zoo <- zoo(newdata, dates)
+    expect_error(predict.ardl(ardl_zoo, newdata_zoo))
+    newdata_zooreg <- zooreg(newdata, start = c(1995, 2), frequency = 4)
+    expect_error(predict.ardl(ardl_zooreg, newdata_zooreg))
+    # As a double check we'll carry out basic check on monthly frequency
+    PSS2001_monthly <- ts(coredata(PSS2001), start = c(1970, 1), frequency = 12)
+    ardl_monthly <- ardl(w ~ Prod + UR + Wedge + Union | D7475 + D7579, order =c(3,0,1,4,3), data = PSS2001_monthly)
+    # Incorrect frequency
+    newdata_ts <- ts(newdata, start = c(1979, 5), frequency = 3)
+    expect_error(predict.ardl(ardl_monthly, newdata_ts))
+    # Not consecutive
+    newdata_ts <- ts(newdata, start = c(1980, 1), frequency = 4)
+    expect_error(predict.ardl(ardl_monthly, newdata_ts))
+})
